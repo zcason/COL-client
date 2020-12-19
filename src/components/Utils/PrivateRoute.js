b9fd7928b816917643c8e66c5437afde9d743860
zcason <zuri.cason@gmail.com>
2020-12-19 15:09:45 -0600

    update the redirect endpoint in private route

--- src/components/Utils/PrivateRoute.js
+++ src/components/Utils/PrivateRoute.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import TokenService from '../../services/token-service'
+import moment from 'moment'
 
 export default function PublicOnlyRoute({ component, ...props }) {
     const Component = component
+    const beginDate = moment().startOf('month').format('YYYY-MM-DD')
+    const endDate = moment().endOf('month').format('YYYY-MM-DD')
     return (
         <Route
             {...props}
             render={componentProps => (
                 TokenService.hasAuthToken()
-                    ? <Redirect to={'/home'} />
+                    ? <Redirect to={`/home/${beginDate}/${endDate}`} />
                     : <Component {...componentProps} />
             )}
         />
