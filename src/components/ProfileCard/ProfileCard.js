e8cc1f69c17e6452dc22a0b03fbff9597df9f738
zcason <zuri.cason@gmail.com>
2021-06-01 00:46:53 -0500

    made buttons for submiting and deleting numbers

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import colApiServices from '../../services/col-api-service';
 import TokenService from '../../services/token-service';
+import { Input } from '../Utils/Utils';
 import './ProfileCard.css';
 
 
@@ -11,7 +12,8 @@ class ProfileCard extends Component {
         userEmail: "",
         accountDeleted: false,
         addingNumber: false,
-        phoneNumber: null
+        phoneNumber: null,
+        errorMessage: null
     }
 
     componentDidMount() {
@@ -26,6 +28,16 @@ class ProfileCard extends Component {
         this.setState({ addingNumber: true})
     }
 
+    handleNumberValidation = (phone) => {
+        const phoneNumber = parseInt(phone);
+
+        if (!Number.isInteger(phoneNumber)) {
+            this.setState({errorMessage: "Phone number should only contain number."})
+        } else {
+            this.setState({ errorMessage: null})
+        }
+    }
+
     handleDeleteProfile = (event) => {
         event.preventDefault();
 
@@ -35,21 +47,49 @@ class ProfileCard extends Component {
     }
 
     render() {
-        const { userName, userEmail, accountDeleted, addingNumber, phoneNumber} = this.state;
+        const { userName, userEmail, accountDeleted, addingNumber, phoneNumber, errorMessage } = this.state;
 
+        const phoneNumberInput = () => {
+            return <div className='phone-number-box'>
+                    <p>Phone Number:</p>
+                    <Input
+                        onChange={e => this.handleNumberValidation(e.target.value)}
+                        name='phone_number'
+                        id='phone_number'
+                        placeholder="Enter phone number"
+                        type='text'
+                        minLength='10'
+                        maxLength='10'
+                        required
+                    />
+                </div>
+            }
+
+        const numberButtons = () => {
+            if (!phoneNumber && addingNumber === false) {
+               return <button onClick={this.handleAddNumber}>Add Phone Number</button>
+            }
+            if (!phoneNumber && addingNumber === true) {
+                return <button onClick={this.handleSubmitNumber}>Submit</button>
+            }
+            if (phoneNumber && addingNumber === false) {
+                <button onClick={this.handleDeleteNumber}>Delete</button>
+            }
+        };
 
         return (
             <>
                 { accountDeleted && (<Redirect to={'/'} />)}
-                <h2 className="profile-title">Profile</h2>
-                <div className="profile-card">
-                    <div className="user-info">
+                <h2 className='profile-title'>Profile</h2>
+                <div className='profile-card'>
+                    <div className='user-info'>
                         <p>{userName}</p>
                         <p>{userEmail}</p>
                     </div >
-                    {}
-                    <div className="profile-buttons">
-                        <button onClick={this.handleAddNumber}>Add Phone Number</button>
+                    {addingNumber && phoneNumberInput()}
+                    {errorMessage && <p className="error-message">{errorMessage}</p> }
+                    <div className='profile-buttons'>
+                        {numberButtons()}
                         <button onClick={this.handleDeleteProfile}>Delete</button>
                     </div>
                 </div>
